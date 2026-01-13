67c1811442fc720bc0a7ce5030e5ae8282f00c6f
GitHub Action <action@github.com>
2026-01-13 07:12:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/ohaddahan/jetbrainscircomsyntaxhighlightplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ohaddahan/jetbrainscircomsyntaxhighlightplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ohaddahan.jetbrainscircomsyntaxhighlightplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ohaddahan.jetbrainscircomsyntaxhighlightplugin.MyBundle
+import com.github.ohaddahan.jetbrainscircomsyntaxhighlightplugin.services.MyProjectService
 import javax.swing.JButton
 
 
